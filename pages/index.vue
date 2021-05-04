64762230e1c24ff9904d87562d05ca8f96fd9eb0
krpk1900 <krpk1900@gmail.com>
2021-05-04 12:30:07 +0900

    [Fix] フェードインアニメーションのトリガーのスクロール幅を画面の縦幅に依存するように修正

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12">
       <v-system-bar color="#fffaf0" app height="50vh">
-        <p>{{ scrollY }}</p>
+        <p>scrollY = {{ scrollY }} </p>
         <nuxt-link class="link leftmost-link" v-scroll-to="'#title'" to>Top</nuxt-link>
         <nuxt-link class="link" v-scroll-to="'#profile'" to>Profile</nuxt-link>
         <nuxt-link class="link" v-scroll-to="'#works'" to>Works</nuxt-link>
@@ -14,27 +14,27 @@
 
       <div class="title" id="title">
         <img class="code-img" src="code5.jpg" alt="">
-        <vue-typer class="typing" text="Terai Shogo's Portfolio" repeat='0' pre-type-delay="100" type-delay="80"></vue-typer>
+        <vue-typer class="typing" text="Terai Shogo's Portfolio" repeat="0" pre-type-delay="100" type-delay="80"></vue-typer>
       </div>
 
       <div class="profile" id="profile">
-        <h2 v-show="isCreated" class="animate__animated" :class="{animate__fadeInUp: isCreated}">Profile</h2>
+        <h2 v-show="isFadeInUp1" class="animate__animated" :class="{animate__fadeInUp: isFadeInUp1}">Profile</h2>
       </div>
 
       <div class="works" id="works">
-        <h2 v-show="isFadeInUp1" class="animate__animated" :class="{animate__fadeInUp: isFadeInUp1}">Works</h2>
+        <h2 v-show="isFadeInUp2" class="animate__animated" :class="{animate__fadeInUp: isFadeInUp2}">Works</h2>
       </div>
 
       <div class="skill" id="skill">
-        <h2 v-show="isFadeInUp2" class="animate__animated" :class="{animate__fadeInUp: isFadeInUp2}">Skill</h2>
+        <h2 v-show="isFadeInUp3" class="animate__animated" :class="{animate__fadeInUp: isFadeInUp3}">Skill</h2>
       </div>
 
       <div class="history" id="history">
-        <h2 v-show="isFadeInUp3" class="animate__animated" :class="{animate__fadeInUp: isFadeInUp3}">History</h2>
+        <h2 v-show="isFadeInUp4" class="animate__animated" :class="{animate__fadeInUp: isFadeInUp4}">History</h2>
 
         <v-timeline align-top :dense="$vuetify.breakpoint.smAndDown">
           <v-timeline-item v-for="(item, i) in items" :key="i" :color="item.color" :icon="item.icon" fill-dot>
-            <v-card :color="item.color" dark :class="[{right: i%2==0}, {left: i%2==1}, {animate__fadeInUp: isFadeInUp2}]" class="animate__animated">
+            <v-card :color="item.color" dark :class="[{right: i%2==0}, {left: i%2==1}, {animate__fadeInUp: isFadeInUp4}]" class="animate__animated">
               <v-card-title class="title">Lorem Ipsum Dolor</v-card-title>
               <v-card-text class="white text--primary">
                 <p>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</p>
@@ -58,7 +58,7 @@
   }
   .code-img {
     width: 100%;
-    height: 90vh;
+    height: 100vh;
   }
   .title {
     position: relative;
@@ -153,11 +153,14 @@ export default {
     },
     methods: {
       handleScroll() {
+        this.innnerHeight = window.innerHeight;
+        console.log(window.innerHeight);
         this.scrollY = window.scrollY;
-        this.isFadeInUp1 = window.scrollY > 930;
-        this.isFadeInUp2 = window.scrollY > 1880;
-        this.isFadeInUp3 = window.scrollY > 2830;
+        this.isFadeInUp1 = window.scrollY > 80;
+        this.isFadeInUp2 = window.scrollY > window.innerHeight + 80;
+        this.isFadeInUp3 = window.scrollY > window.innerHeight*2 + 80;
+        this.isFadeInUp4 = window.scrollY > window.innerHeight*3 + 80;
       },
-    }
+    },
   }
 </script>
